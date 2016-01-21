48190191e63370747ee3a39652fb436589d652e5
Cabané <jorgecabane93@gmail.com>
2016-01-21 13:09:43 -0300

    sessions

--- www/js/loading.js
+++ www/js/loading.js
@@ -5,6 +5,7 @@
 $(document).ready(function () {
     $(document).ajaxStart(function () {
         $("#loading").show();
+        $(".container").hide();
     }).ajaxStop(function () {
         $("#loading").hide();
         $(".container").show();
